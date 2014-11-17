02d83adf83d6323e4a581cecb1bdcafd051154e1
Gus Donnelly <gmd68@cornell.edu>
2014-11-17 17:30:03 -0500

    Changed directory structrue so that we can easily identify what is ours and added LocalTileSource, which will replace the OSMTileSource, etc. that references online media. Also separated GUI (now called GUIMain in the gui directory) into tabs; working on adding buttons & functionality to the control tab.

---
+++ src/gui/LocalTileSource.java
@@ -0,0 +1,17 @@
+package gui;
+
+import org.openstreetmap.gui.jmapviewer.tilesources.AbstractTMSTileSource;
+
+public class LocalTileSource extends AbstractTMSTileSource {
+
+	
+	public LocalTileSource () {
+		super();
+	}
+	@Override
+	public TileUpdate getTileUpdate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
